c54b74f258bc3f299b547199453718c978876be5
igorzakhar <igorzakhar@gmail.com>
2018-09-13 14:54:55 +0300

    Second iteration

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,20 +1,28 @@
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
 
-var scores = map[string]int{"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2,
-	"F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3,
-	"L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1,
-	"R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4,
-	"X": 8, "Z": 10}
-
 func Score(word string) int {
-	var points int
-	wordUpper := strings.ToUpper(word)
-	for _, char := range wordUpper {
-		points += scores[string(char)]
+	points := 0
+	for _, letter := range word {
+		switch unicode.ToLower(letter) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			points += 1
+		case 'd', 'g':
+			points += 2
+		case 'b', 'c', 'm', 'p':
+			points += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			points += 4
+		case 'k':
+			points += 5
+		case 'j', 'x':
+			points += 8
+		case 'q', 'z':
+			points += 10
+		}
 	}
 	return points
 }
